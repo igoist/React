1a02eb3cf3709a7bf7e6e12903082a41f5ffcc13
XuTianZhu <493512132@qq.com>
2017-04-02 22:30:51 +0800

    The initial work for my first react app ---- For exploring React & Zent

---
+++ demoApps/hello-world/README.md
@@ -0,0 +1,1 @@
+## Try about React
---
+++ demoApps/hello-world/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from 'zent';
+
+// import 'zent/assets/index.scss';
+import 'zent/lib/index.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button>Zent</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
