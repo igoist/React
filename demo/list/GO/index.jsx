61765ca1c375f9f961a7de7c92c42517cf5c2fcc
igoist <493512132@qq.com>
2017-06-29 20:49:28 +0800

    dev-chess: 根据 rows 自动生成数组、行内样式（宽度），scss 中加入 .chess:after 的 transition 属性

--- demo/list/GO/index.jsx
+++ demo/list/GO/index.jsx
@@ -1,3 +1,17 @@
+const rows = 5
+const t = (x) => Math.floor((x * Math.random()))
+const boxSize = rows * (32 + 6 * 2)
+
+// var arr = [0, 1, 1, 1, 0, 1, 1, 1, 0]
+let arr = (rows) => {
+  let arr = []
+  for (let i = 0; i < rows * rows; i++) {
+    arr.push(t(100) % 2)
+  }
+}
+
+console.log('finish arr initial: ' + arr)
+
 class Chess extends React.Component {
   constructor(props) {
     super(props)
@@ -12,7 +26,7 @@ class Chess extends React.Component {
       flag: !this.state.flag
     })
   }
-
+  // handleChessClick
 
   render() {
     let str = this.state.flag > 0 ? 'chess flag' : 'chess'
@@ -26,31 +40,51 @@ class Chess extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.arr = this.props.arr
+    this.rows = this.props.rows
     console.log(this.props.arr)
     this.handleChess = this.handleChess.bind(this)
   }
 
-  handleChess(i) {
-    this.arr
+  handleChess(x, y) {
+    let index = x * this.rows + y
+    if (this.arr[index]) {
+      this.arr[index] = 0
+    } else {
+      this.arr[index] = 1
+    }
+    console.log(this.arr[index])
   }
 
   render() {
+    var inlineStyle = {
+      width: boxSize + 'px'
+    }
+
     let rows = [];
-    this.props.arr.map((i, index) => {
-      let f = i > 0 ? true : false
-      rows.push(<Chess id={index} flag={f} key={index.toString()} />)
-    })
+    // this.props.arr.map((i, index) => {
+    //   let f = i > 0 ? true : false
+    //   rows.push(<Chess id={index} flag={f} key={index.toString()} />)
+    // })
+    for (let x = 0; x < this.props.rows; x++) {
+      for (let y = 0; y < this.props.rows; y++) {
+        let index = x * this.rows + y
+        let f = this.arr[index] > 0 ? true : false 
+        rows.push(<Chess id={index} flag={f} key={index.toString()} />)
+      }
+    }
+
     return (
-      <div id="board">
+      <div id="board" style={inlineStyle}>
         {rows}
       </div>
     )
   }
 }
 
-var arr = [0, 1, 1, 1, 0, 1, 1, 1, 0]
+
 
 ReactDOM.render(
-  <App arr={arr} />,
+  <App arr={arr} rows={rows} />,
   document.getElementById('root')
 )
